398419f1fddd6388d0150091a629cb7f633bfcb9
Burneikis <alexburneikis@gmail.com>
2025-07-31 19:42:08 +1000

    style positioning mag glass

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ function App() {
   return (
     <div className="App">
       <div className="home-container">
-        <h1 className="app-title">Youtube Channel Filterer</h1>
-        <div className="search-container">
-          <input 
-            type="text" 
-            className="search-bar" 
-            placeholder="Choose a channel"
-          />
+        <div className="content-center">
+          <h1 className="app-title">Youtube Channel Filterer</h1>
+          <div className="search-container">
+            <div className="search-input-wrapper">
+              <svg className="search-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M21 21L16.514 16.506L21 21ZM19 10.5C19 15.194 15.194 19 10.5 19C5.806 19 2 15.194 2 10.5C2 5.806 5.806 2 10.5 2C15.194 2 19 5.806 19 10.5Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
+              <input
+                type="text"
+                className="search-bar"
+                placeholder="Choose a channel"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
